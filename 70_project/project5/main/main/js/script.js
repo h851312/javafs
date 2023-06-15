6806c9530f97b742f9bdc34c0fb91099fd48429b
h851312 <h851312@gmail.com>
2023-06-15 17:53:41 +0900

    0615

--- 70_project/project5/main/main/js/script.js
+++ 70_project/project5/main/main/js/script.js
@@ -104,45 +104,99 @@ $(function () {
             $('html').stop().animate({ scrollTop: next }, 400, 'easeOutExpo');
         }
     });
-});
-//섹션2
-const sec2 = $('#section2'),
-    btn = sec2.find('.btn'),
-    txt1 = sec2.find('.txt1'),
-    txt2 = sec2.find('.txt2');
+    //섹션2
+    const sec2 = $('#section2'),
+        btn = sec2.find('.btn'),
+        txt1 = sec2.find('.txt1'),
+        txt2 = sec2.find('.txt2');
 
-$(window).scroll(function () {
-    let sT = $(this).scrollTop();
-    let stVal = 600;
-    console.log(sT);
+    $(window).scroll(function () {
+        let sT = $(this).scrollTop();
+        let stVal = 600;
+        console.log(sT);
 
-    if (sT >= stVal) {
-        btn.css({ opacity: 1 });
-        txt1.css({ left: 360 + 'px' });
-        // txt.style.left = 400 + 'px';
-        txt2.css({ left: 360 + 'px' });
+        if (sT >= stVal) {
+            btn.css({ opacity: 1 });
+            txt1.css({ left: 360 + 'px' });
+            // txt.style.left = 400 + 'px';
+            txt2.css({ left: 360 + 'px' });
 
-    } else {
-        btn.css({ opacity: 0 });
-        txt1.css({ left: -800 + 'px' });
-        // txt.style.left = -400 + 'px';
-        txt2.css({ left: -400 + 'px' });
-    }
-});
+        } else {
+            btn.css({ opacity: 0 });
+            txt1.css({ left: -800 + 'px' });
+            // txt.style.left = -400 + 'px';
+            txt2.css({ left: -400 + 'px' });
+        }
+    });
 
-// 섹션3
+    // 섹션3
 
-const tabBtn = $('.thumb li'),
-    bigImg = $('.big li'),
-    txt = $('.innerWrap .txt li');
+    const tabBtn = $('.thumb li'),
+        bigImg = $('.big li'),
+        txt = $('.txt li');
 
-tabBtn.click(function () {
-    let idx = $(this).index();
-    tabBtn.removeClass('active');
-    bigImg.removeClass('active');
-    txt.removeClass('active');
-    // $(this).addClass('active');
-    // bigImg.eq(idx).addClass('active');
-    // txt.eq(idx).addClass('active');
+    tabBtn.click(function () {
+        let idx = $(this).index();
+        tabBtn.removeClass('active');
+        bigImg.removeClass('active');
+        txt.removeClass('active');
+        $(this).addClass('active');
+        bigImg.eq(idx).addClass('active');
+        txt.eq(idx).addClass('active');
+
+    });
+
+    //섹션4
+
+    //상단 텍스트 모션을 위한 텍스트 복제
+    const txtTop = $('.top');
+    const txtTopSpan = txtTop.find('span');
+    const txtBtm = $('.btm');
+    const txtBtmSpan = txtBtm.find('span');
+
+    txtTopSpan.clone().appendTo(txtTop);
+    txtBtmSpan.clone().appendTo(txtBtm);
+
+    //호버 페이드 이펙트 
+
+    // var cnt = 0, timer, idx;
+    // $('.fade').mouseenter(function(){
+    //     idx = $(this).parent().index();
+    //     timer = setInterval(slide, 2000);
+    // });
+    // $('fade').mouseleave(function(){
+    //     clearInterval(timer);
+    // });
+    const inner = $('.container > div');
+    const fade = $('.fade');
+    let cnt = 0, idx, timer;
+
+    fade.mouseenter(function () {
+        //마우스를 올린 fade의 부모의 색인 번호를 idx 저장
+        idx = $(this).parent().index();
+        //2초마다 fadeFn 함수를 호출한다.
+        timer = setInterval(fadeFn, 2000)
+    });
+
+    //fade 위에서  마우스가 벗어나면 함수 실행
+    fade.mouseleave(function () {
+        //setinterval 정지
+        clearInterval(timer);
+    });
+    function fadeFn() {
+        cnt++;
+        if (cnt > 2) {
+            cnt = 0;
+        }
+        //idx가 0이명 .story
+        //idxrk 1이면 .identity
+        inner.eq(idx).find('li').eq(cnt).fadeIn(1000).siblings().fadeOut(1000);
+        // inner.eq(idx).find('li').fadeOut(1000);
+        // inner.eq(idx).find('li').eq(cnt).fadeIn(1000);
+
+    };
+
+
+
 
 });
