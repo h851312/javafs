e6608bf05e81bab02c89840507e3450bf81f6dde
h851312 <h851312@gmail.com>
2023-06-27 09:42:33 +0900

    0627

---
+++ workspace/vscode/HELLO/hello/src/Substring1.java
@@ -0,0 +1,7 @@
+public class Substring1 {
+    public static void main(String[] args) {
+        
+        String a = "Hello Java";
+        System.out.println(a.substring(0, 4)); //Hell출력
+    }
+}
