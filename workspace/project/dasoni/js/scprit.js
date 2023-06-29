67ac457a8a20117b3246464d9fce7391a33b69c3
h851312 <h851312@gmail.com>
2023-06-29 17:50:34 +0900

    0629

---
+++ workspace/project/dasoni/js/scprit.js
@@ -0,0 +1,4 @@
+function titMotion() {
+    // 슬라이드 전환 직후의 텍스트 모션
+    $('#slideWrap .slider li div').animate({ top: 450, opacity: 1 });
+}
---
+++ workspace/vscode/HELLO/hello/src/a0629/Ex4_12.java
@@ -0,0 +1,11 @@
+package a0629;
+
+public class Ex4_12 {
+    public static void main(String[] args) {
+        int i =1;
+        while (i<=10) {
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+}
