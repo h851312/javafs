ee48d97124a991f0bc337e12266e0891a48dd09f
h851312 <h851312@gmail.com>
2023-06-30 17:50:36 +0900

    0630

---
+++ workspace/vscode/HELLO/hello/src/a0630/Ex4_3.java
@@ -0,0 +1,14 @@
+package a0630;
+
+public class Ex4_3 {
+    public static void main(String[] args) {
+        int sum =0;
+        int totalSum = 0;
+        for(int i=1; i<=10; i++){
+            sum+= i;
+            totalSum =  totalSum + sum;
+        }
+        System.out.println("sum:" + sum);
+        System.out.println("totalSum:" + totalSum);
+    }
+}
